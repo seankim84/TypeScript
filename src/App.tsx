48543399fb565a95878260a2061616112f4f1d37
SeanKim <sean.kim84@gmail.com>
2018-07-06 13:57:44 +0700

    Only Uing the TypeScript for make the TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Counter from "./components/Counter";
 import Profile from "./components/Profile";
-
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
   render() {
@@ -9,6 +9,7 @@ class App extends React.Component {
       <div>
         <Profile name="벨로퍼트" job="코드사랑꾼" />
         <Counter />
+        <TodoList />
       </div>
     );
   }
